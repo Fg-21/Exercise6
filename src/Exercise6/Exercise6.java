b935a4cc1ebc1524ae270efc623b4e8363ea2a75
fm.gonzalez <fm.gonzalez@106-12.info2>
2024-09-20 14:21:05 +0200

    Exercise6

---
+++ src/Exercise6/Exercise6.java
@@ -0,0 +1,12 @@
+package Exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("Francisco Manuel");
+		System.out.println("Vistahermosa 59");
+		System.out.println("");
+		System.out.println("657085306");
+	}
+
+}
